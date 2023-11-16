fa4c2adf59d8da68f6d22bd5063262d6f0dd9945
Rouzzen <vicente.neira@usm.cl>
2023-11-15 23:13:48 -0300

    funciones básicas

--- Fulcrum3/fulcrum3.go
+++ Fulcrum3/fulcrum3.go
@@ -4,9 +4,11 @@ import (
 	pb "central/github.com/Sistemas-Distribuidos-2023-02/Grupo10-Laboratorio-3" // Asegúrate de ajustar la importación correctamente
 	"context"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net"
 	"os"
+	"strings"
 
 	"google.golang.org/grpc"
 )
@@ -48,23 +50,83 @@ func (s *baseServiceServer) AgregarBase(ctx context.Context, req *pb.AgregarBase
 }
 
 func (s *baseServiceServer) RenombrarBase(ctx context.Context, req *pb.RenombrarBaseRequest) (*pb.Respuesta, error) {
-	// Lógica para el comando AgregarBase
-	// ...
-
+	nombresector := fmt.Sprintf("Sector%s.txt", req.NombreSector)
+	if _, err := os.Stat(nombresector); os.IsNotExist(err) {
+		//Hay que agregar lo de los logs
+		return &pb.Respuesta{Mensaje: "Comando RenombrarBase ejecutado", Exitoso: true}, nil
+	} else {
+		contenido, err := ioutil.ReadFile(nombresector)
+		if err != nil {
+			return &pb.Respuesta{Mensaje: "Comando RenombrarBase no pudo ser ejecutado", Exitoso: false}, err
+		}
+		lineas := strings.Split(string(contenido), "\n")
+		for i, linea := range lineas {
+			if strings.Contains(linea, req.NombreBase) {
+				lineas[i] = strings.Replace(linea, req.NombreBase, req.NuevoNombre, -1)
+				break
+			}
+		}
+		nuevoContenido := strings.Join(lineas, "\n")
+		err = ioutil.WriteFile(nombresector, []byte(nuevoContenido), 0644)
+		if err != nil {
+			return &pb.Respuesta{Mensaje: "Comando RenombrarBase no pudo ser ejecutado", Exitoso: false}, err
+		}
+	}
 	return &pb.Respuesta{Mensaje: "Comando RenombrarBase ejecutado", Exitoso: true}, nil
 }
 
 func (s *baseServiceServer) ActualizarValor(ctx context.Context, req *pb.ActualizarValorRequest) (*pb.Respuesta, error) {
-	// Lógica para el comando AgregarBase
-	// ...
+	nombresector := fmt.Sprintf("Sector%s.txt", req.NombreSector)
+	if _, err := os.Stat(nombresector); os.IsNotExist(err) {
+		//Hay que agregar lo de los logs
+		return &pb.Respuesta{Mensaje: "Comando ActualizaValor ejecutado", Exitoso: true}, nil
+	} else {
+		contenido, err := ioutil.ReadFile(nombresector)
+		if err != nil {
+			return &pb.Respuesta{Mensaje: "Comando ActualizaValor no pudo ser ejecutado", Exitoso: false}, err
+		}
+		lineas := strings.Split(string(contenido), "\n")
+		for i, linea := range lineas {
+			if strings.Contains(linea, req.NombreBase) {
+				nuevaLinea := fmt.Sprintf("Sector %s %s %.0f\n", req.NombreSector, req.NombreBase, req.NuevoValor)
+				lineas[i] = nuevaLinea
+				break
+			}
+		}
+		nuevoContenido := strings.Join(lineas, "\n")
 
+		err = ioutil.WriteFile(nombresector, []byte(nuevoContenido), 0644)
+		if err != nil {
+			return &pb.Respuesta{Mensaje: "Comando ActualizaValor no pudo ser ejecutado", Exitoso: false}, err
+		}
+	}
 	return &pb.Respuesta{Mensaje: "Comando ActualizaValor ejecutado", Exitoso: true}, nil
 }
 
 func (s *baseServiceServer) BorrarBase(ctx context.Context, req *pb.BorrarBaseRequest) (*pb.Respuesta, error) {
-	// Lógica para el comando AgregarBase
-	// ...
+	nombresector := fmt.Sprintf("Sector%s.txt", req.NombreSector)
+	if _, err := os.Stat(nombresector); os.IsNotExist(err) {
+		//Hay que agregar lo de los logs
+		return &pb.Respuesta{Mensaje: "Comando BorrarBase ejecutado", Exitoso: true}, nil
+	} else {
+		contenido, err := ioutil.ReadFile(nombresector)
+		if err != nil {
+			return &pb.Respuesta{Mensaje: "Comando BorrarBase no pudo ser ejecutado", Exitoso: false}, err
+		}
+		lineas := strings.Split(string(contenido), "\n")
+		var nuevasLineas []string
+		for _, linea := range lineas {
+			if !strings.Contains(linea, req.NombreBase) {
+				nuevasLineas = append(nuevasLineas, linea)
+			}
+		}
+		nuevoContenido := strings.Join(nuevasLineas, "\n")
 
+		err = ioutil.WriteFile(nombresector, []byte(nuevoContenido), 0644)
+		if err != nil {
+			return &pb.Respuesta{Mensaje: "Comando BorrarBase no pudo ser ejecutado", Exitoso: false}, err
+		}
+	}
 	return &pb.Respuesta{Mensaje: "Comando BorrarBase ejecutado", Exitoso: true}, nil
 }
 
