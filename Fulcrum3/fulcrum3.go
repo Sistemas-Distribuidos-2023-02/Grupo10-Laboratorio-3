125f8937910d8c4ffd98a030fd27406f9528437b
alvarosooto <alvaro.sotope@usm.cl>
2023-11-18 18:44:30 -0300

    no mas SECTOR

--- Fulcrum3/fulcrum3.go
+++ Fulcrum3/fulcrum3.go
@@ -86,22 +86,22 @@ func (s *baseServiceServer) AgregarLOG(info, base, sector, nuevonombre string, v
 
 	switch info { // Escribir la información de la base en el log
 	case "agregar":
-		_, err = fmt.Fprintf(logfile, "AgregarBase Sector %s %s %.0f\n", base, sector, valor)
+		_, err = fmt.Fprintf(logfile, "AgregarBase %s %s %.0f\n", base, sector, valor)
 		if err != nil {
 			return &pb.Respuesta{Mensaje: "No pudo escribirse correctamente en archivo log", Exitoso: false}, err
 		}
 	case "renombrar":
-		_, err = fmt.Fprintf(logfile, "RenombrarBase Sector %s %s %s\n", base, sector, nuevonombre)
+		_, err = fmt.Fprintf(logfile, "RenombrarBase %s %s %s\n", base, sector, nuevonombre)
 		if err != nil {
 			return &pb.Respuesta{Mensaje: "No pudo escribirse correctamente en archivo log", Exitoso: false}, err
 		}
 	case "actualizar":
-		_, err = fmt.Fprintf(logfile, "ActualizarBase Sector %s %s %.0f\n", base, sector, nuevovalor)
+		_, err = fmt.Fprintf(logfile, "ActualizarBase %s %s %.0f\n", base, sector, nuevovalor)
 		if err != nil {
 			return &pb.Respuesta{Mensaje: "No pudo escribirse correctamente en archivo log", Exitoso: false}, err
 		}
 	case "borrar":
-		_, err = fmt.Fprintf(logfile, "BorrarBase Sector %s %s \n", base, sector)
+		_, err = fmt.Fprintf(logfile, "BorrarBase %s %s \n", base, sector)
 		if err != nil {
 			return &pb.Respuesta{Mensaje: "No pudo escribirse correctamente en archivo log", Exitoso: false}, err
 		}
@@ -112,7 +112,7 @@ func (s *baseServiceServer) AgregarLOG(info, base, sector, nuevonombre string, v
 var n int
 
 func (s *baseServiceServer) AgregarBase(ctx context.Context, req *pb.AgregarBaseRequest) (*pb.Respuesta, error) {
-	nombreArchivo := fmt.Sprintf("Sector%s.txt", req.NombreSector)
+	nombreArchivo := fmt.Sprintf("%s.txt", req.NombreSector)
 	if _, err := os.Stat(nombreArchivo); os.IsNotExist(err) {
 		// El archivo no existe, entonces se crea uno nuevo
 		if err := s.CrearRegistro(nombreArchivo); err != nil {
@@ -130,7 +130,7 @@ func (s *baseServiceServer) AgregarBase(ctx context.Context, req *pb.AgregarBase
 			return &pb.Respuesta{Mensaje: "No pudo escribirse correctamente en archivo de sector", Exitoso: false}, err
 		}
 		// Escribir la información de la base en el archivo
-		_, err = fmt.Fprintf(file, "Sector %s %s %.0f", req.NombreSector, req.NombreBase, req.Valor)
+		_, err = fmt.Fprintf(file, "%s %s %.0f", req.NombreSector, req.NombreBase, req.Valor)
 		if err != nil {
 			return &pb.Respuesta{Mensaje: "No pudo escribirse correctamente en archivo de sector", Exitoso: false}, err
 		}
@@ -154,7 +154,7 @@ func (s *baseServiceServer) AgregarBase(ctx context.Context, req *pb.AgregarBase
 	}
 
 	// Escribir la información de la base en el archivo
-	_, err = fmt.Fprintf(file, "Sector %s %s %.0f", req.NombreSector, req.NombreBase, req.Valor)
+	_, err = fmt.Fprintf(file, "%s %s %.0f", req.NombreSector, req.NombreBase, req.Valor)
 	if err != nil {
 		return &pb.Respuesta{Mensaje: "No pudo escribirse correctamente en archivo de sector", Exitoso: false}, err
 	}
@@ -166,7 +166,7 @@ func (s *baseServiceServer) AgregarBase(ctx context.Context, req *pb.AgregarBase
 	defer logfile.Close()
 
 	// Escribir la información de la base en el log
-	_, err = fmt.Fprintf(logfile, "AgregarBase Sector %s %s %.0f\n", req.NombreSector, req.NombreBase, req.Valor)
+	_, err = fmt.Fprintf(logfile, "AgregarBase %s %s %.0f\n", req.NombreSector, req.NombreBase, req.Valor)
 	if err != nil {
 		return &pb.Respuesta{Mensaje: "No pudo escribirse correctamente en archivo log", Exitoso: false}, err
 	}
@@ -174,7 +174,7 @@ func (s *baseServiceServer) AgregarBase(ctx context.Context, req *pb.AgregarBase
 }
 
 func (s *baseServiceServer) RenombrarBase(ctx context.Context, req *pb.RenombrarBaseRequest) (*pb.Respuesta, error) {
-	nombreArchivo := fmt.Sprintf("Sector%s.txt", req.NombreSector)
+	nombreArchivo := fmt.Sprintf("%s.txt", req.NombreSector)
 	if _, err := os.Stat(nombreArchivo); os.IsNotExist(err) {
 		//Hay que agregar lo de los logs
 		// El archivo no existe, entonces se crea uno nuevo
@@ -193,7 +193,7 @@ func (s *baseServiceServer) RenombrarBase(ctx context.Context, req *pb.Renombrar
 			return &pb.Respuesta{Mensaje: "No pudo escribirse correctamente en archivo de sector", Exitoso: false}, err
 		}
 		// Escribir la nueva línea en el archivo
-		nuevaLinea := fmt.Sprintf("Sector %s %s 0", req.NombreSector, req.NuevoNombre)
+		nuevaLinea := fmt.Sprintf("%s %s 0", req.NombreSector, req.NuevoNombre)
 		_, err = file.WriteString(nuevaLinea)
 		if err != nil {
 			log.Fatalf("Error al escribir en el archivo %s: %v", nombreArchivo, err)
@@ -214,7 +214,7 @@ func (s *baseServiceServer) RenombrarBase(ctx context.Context, req *pb.Renombrar
 			}
 		}
 		if encontrar == false { //No se encontró la base en el sector
-			nuevalinea := fmt.Sprintf("Sector %s %s 0", req.NombreSector, req.NuevoNombre)
+			nuevalinea := fmt.Sprintf("%s %s 0", req.NombreSector, req.NuevoNombre)
 			lineas = append(lineas, nuevalinea)
 		}
 		nuevoContenido := strings.Join(lineas, "\n")
@@ -237,7 +237,7 @@ func (s *baseServiceServer) RenombrarBase(ctx context.Context, req *pb.Renombrar
 }
 
 func (s *baseServiceServer) ActualizarValor(ctx context.Context, req *pb.ActualizarValorRequest) (*pb.Respuesta, error) {
-	nombreArchivo := fmt.Sprintf("Sector%s.txt", req.NombreSector)
+	nombreArchivo := fmt.Sprintf("%s.txt", req.NombreSector)
 	if _, err := os.Stat(nombreArchivo); os.IsNotExist(err) {
 		//Hay que agregar lo de los logs
 		if err := s.CrearRegistro(nombreArchivo); err != nil {
@@ -255,7 +255,7 @@ func (s *baseServiceServer) ActualizarValor(ctx context.Context, req *pb.Actuali
 			return &pb.Respuesta{Mensaje: "No pudo escribirse correctamente en archivo de sector", Exitoso: false}, err
 		}
 		// Escribir la nueva línea en el archivo
-		nuevaLinea := fmt.Sprintf("Sector %s %s %.0f", req.NombreSector, req.NombreBase, req.NuevoValor)
+		nuevaLinea := fmt.Sprintf("%s %s %.0f", req.NombreSector, req.NombreBase, req.NuevoValor)
 		_, err = file.WriteString(nuevaLinea)
 		if err != nil {
 			log.Fatalf("Error al escribir en el archivo %s: %v", nombreArchivo, err)
@@ -270,14 +270,14 @@ func (s *baseServiceServer) ActualizarValor(ctx context.Context, req *pb.Actuali
 		lineas := strings.Split(string(contenido), "\n")
 		for i, linea := range lineas {
 			if strings.Contains(linea, req.NombreBase) {
-				nuevaLinea := fmt.Sprintf("Sector %s %s %.0f", req.NombreSector, req.NombreBase, req.NuevoValor)
+				nuevaLinea := fmt.Sprintf("%s %s %.0f", req.NombreSector, req.NombreBase, req.NuevoValor)
 				lineas[i] = nuevaLinea
 				encontrar = true
 				break
 			}
 		}
 		if encontrar == false {
-			nuevalinea := fmt.Sprintf("Sector %s %s %.0f", req.NombreSector, req.NombreBase, req.NuevoValor)
+			nuevalinea := fmt.Sprintf("%s %s %.0f", req.NombreSector, req.NombreBase, req.NuevoValor)
 			lineas = append(lineas, nuevalinea)
 		}
 		nuevoContenido := strings.Join(lineas, "\n")
@@ -301,7 +301,7 @@ func (s *baseServiceServer) ActualizarValor(ctx context.Context, req *pb.Actuali
 }
 
 func (s *baseServiceServer) BorrarBase(ctx context.Context, req *pb.BorrarBaseRequest) (*pb.Respuesta, error) {
-	nombreArchivo := fmt.Sprintf("Sector%s.txt", req.NombreSector)
+	nombreArchivo := fmt.Sprintf("%s.txt", req.NombreSector)
 	if _, err := os.Stat(nombreArchivo); os.IsNotExist(err) {
 		// El archivo no existe, entonces se crea uno nuevo
 		if err := s.CrearRegistro(nombreArchivo); err != nil {
@@ -357,7 +357,7 @@ func (s *baseServiceServer) BorrarBase(ctx context.Context, req *pb.BorrarBaseRe
 
 func (s *baseServiceServer) GetSoldados(ctx context.Context, req *pb.GetSoldadosRequest) (*pb.Respuesta, error) {
 	// Nombre del archivo del sector
-	nombreArchivo := fmt.Sprintf("Sector%s.txt", req.NombreSector)
+	nombreArchivo := fmt.Sprintf("%s.txt", req.NombreSector)
 
 	// Leer el contenido del archivo
 	data, err := ioutil.ReadFile(nombreArchivo)
@@ -373,10 +373,10 @@ func (s *baseServiceServer) GetSoldados(ctx context.Context, req *pb.GetSoldados
 	// Buscar la base en las líneas del archivo
 	for _, linea := range lineas {
 		elementos := strings.Fields(linea)
-		if len(elementos) >= 4 && elementos[0] == "Sector" && elementos[1] == req.NombreSector && elementos[2] == req.NombreBase {
+		if len(elementos) >= 3 && elementos[0] == req.NombreSector && elementos[1] == req.NombreBase {
 			// Encontramos la base, devolver la cantidad de soldados
 			return &pb.Respuesta{
-				Mensaje: elementos[3], Exitoso: true,
+				Mensaje: elementos[2], Exitoso: true,
 			}, nil
 		}
 	}
