e4972fa69d45511a1e9b09eb0d89abcbaef885b5
Rouzzen <vicente.neira@usm.cl>
2023-11-20 00:09:18 -0300

    Osiris con Monotonic Writes

--- Informante2/osiris.go
+++ Informante2/osiris.go
@@ -5,14 +5,79 @@ import (
 	pb "central/github.com/Sistemas-Distribuidos-2023-02/Grupo10-Laboratorio-3" // Asegúrate de ajustar la importación correctamente
 	"context"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 	"strconv"
+	"strings"
 
 	"google.golang.org/grpc"
 )
 
 var PrimeraEscritura = true
+
+func MonotonicWrites(sector, reloj, puerto string) string {
+	contenidos, err := ioutil.ReadFile("registro.txt")
+	if err != nil {
+		return "problemas al abrir el archivo "
+	}
+	// Separar el contenido por líneas
+	lineas := strings.Split(string(contenidos), "\n")
+	for i := range lineas {
+		// Verificar si la línea está vacía
+		if lineas[i] == "" {
+			continue
+		}
+
+		// Separar elementos del archivo
+		partes := strings.Split(lineas[i], " ")
+		if len(partes) < 6 {
+			return "Error de formato en el archivo"
+		}
+
+		sectorA := partes[1]
+		relojA := partes[4]
+		puertoA := partes[5]
+
+		// Separar reloj archivo
+		separar := strings.Split(strings.Trim(relojA, "[]"), ",")
+		numero1, err := strconv.Atoi(strings.TrimSpace(separar[0]))
+		if err != nil {
+			return "problemas al abrir el archivo "
+		}
+		numero2, err := strconv.Atoi(strings.TrimSpace(separar[1]))
+		if err != nil {
+			return "problemas al abrir el archivo "
+		}
+		numero3, err := strconv.Atoi(strings.TrimSpace(separar[2]))
+		if err != nil {
+			return "problemas al abrir el archivo "
+		}
+
+		// Separar reloj a agregar
+		separar = strings.Split(strings.Trim(reloj, "[]"), ",")
+		numero4, err := strconv.Atoi(strings.TrimSpace(separar[0]))
+		if err != nil {
+			return "problemas al abrir el archivo "
+		}
+		numero5, err := strconv.Atoi(strings.TrimSpace(separar[1]))
+		if err != nil {
+			return "problemas al abrir el archivo "
+		}
+		numero6, err := strconv.Atoi(strings.TrimSpace(separar[2]))
+		if err != nil {
+			return "problemas al abrir el archivo "
+		}
+
+		if sectorA == sector && puertoA == puerto {
+			if numero1 > numero4 || numero2 > numero5 || numero3 > numero6 {
+				return "Error de consistencia"
+			}
+		}
+	}
+
+	return "No hay problemas"
+}
 
 func inicializarArchivo() error {
 	// Reinicia el contenido del archivo registro
@@ -58,23 +123,28 @@ func enviarComandoAgregarBase(client pb.MiServicioClient, nombreSector, nombreBa
 		PrimeraEscritura = false
 	}
 	reloj := respFulcrum.Mensaje
-	//Escritura en el registro.txt
-	logfile, err := os.OpenFile("registro.txt", os.O_APPEND|os.O_WRONLY, 0644)
-	if err != nil {
-		fmt.Printf("Log de registro no pudo abrirse exitosamente")
-	}
-	defer logfile.Close()
+	//Metodo Monotonic Writes
+	monotonic := MonotonicWrites(req.NombreSector, reloj, puerto)
+	if monotonic == "No hay problemas" {
+		//Escritura en el registro.txt
+		logfile, err := os.OpenFile("registro.txt", os.O_APPEND|os.O_WRONLY, 0644)
+		if err != nil {
+			fmt.Printf("Log de registro no pudo abrirse exitosamente")
+		}
+		defer logfile.Close()
 
-	_, err = fmt.Fprintf(logfile, "AgregarBase %s %s %.0f %s %s\n", req.NombreSector, req.NombreBase, req.Valor, reloj, puerto)
-	if err != nil {
-		fmt.Printf("No pudo escribirse correctamente en archivo log")
-	}
-	if resp.Exitoso {
-		fmt.Printf("Respuesta del %s: Comando AgregarBase Ejecutado con éxito\n", fulcrum)
+		_, err = fmt.Fprintf(logfile, "AgregarBase %s %s %.0f %s %s\n", req.NombreSector, req.NombreBase, req.Valor, reloj, puerto)
+		if err != nil {
+			fmt.Printf("No pudo escribirse correctamente en archivo log")
+		}
+		if resp.Exitoso {
+			fmt.Printf("Respuesta del %s: Comando AgregarBase Ejecutado con éxito\n", fulcrum)
+		} else {
+			fmt.Printf("Respuesta del %s: Comando AgregarBase no pudo ser ejecutado\n", fulcrum)
+		}
 	} else {
-		fmt.Printf("Respuesta del %s: Comando AgregarBase no pudo ser ejecutado\n", fulcrum)
+		fmt.Printf("No se pudo escribir en el archivo registro.txt debido a que hubo un error de consistencia detectado por Monotonic Writes \n")
 	}
-
 }
 
 func enviarComandoRenombrarBase(client pb.MiServicioClient, nombreSector, nombreBase string, valor interface{}) {
@@ -110,23 +180,28 @@ func enviarComandoRenombrarBase(client pb.MiServicioClient, nombreSector, nombre
 		PrimeraEscritura = false
 	}
 	reloj := respFulcrum.Mensaje
-	//Escritura en el registro.txt
-	logfile, err := os.OpenFile("registro.txt", os.O_APPEND|os.O_WRONLY, 0644)
-	if err != nil {
-		fmt.Printf("Log de registro no pudo abrirse exitosamente")
-	}
-	defer logfile.Close()
+	//Metodo Monotonic Writes
+	monotonic := MonotonicWrites(req.NombreSector, reloj, puerto)
+	if monotonic == "No hay problemas" {
+		//Escritura en el registro.txt
+		logfile, err := os.OpenFile("registro.txt", os.O_APPEND|os.O_WRONLY, 0644)
+		if err != nil {
+			fmt.Printf("Log de registro no pudo abrirse exitosamente")
+		}
+		defer logfile.Close()
 
-	_, err = fmt.Fprintf(logfile, "RenombrarBase %s %s %s %s %s\n", req.NombreSector, req.NombreBase, req.NuevoNombre, reloj, puerto)
-	if err != nil {
-		fmt.Printf("No pudo escribirse correctamente en archivo log")
-	}
-	if resp.Exitoso {
-		fmt.Printf("Respuesta del %s: Comando RenombrarBase Ejecutado con éxito\n", fulcrum)
+		_, err = fmt.Fprintf(logfile, "RenombrarBase %s %s %s %s %s\n", req.NombreSector, req.NombreBase, req.NuevoNombre, reloj, puerto)
+		if err != nil {
+			fmt.Printf("No pudo escribirse correctamente en archivo log")
+		}
+		if resp.Exitoso {
+			fmt.Printf("Respuesta del %s: Comando RenombrarBase Ejecutado con éxito\n", fulcrum)
+		} else {
+			fmt.Printf("Respuesta del %s: Comando RenombrarBase no pudo ser ejecutado\n", fulcrum)
+		}
 	} else {
-		fmt.Printf("Respuesta del %s: Comando RenombrarBase no pudo ser ejecutado\n", fulcrum)
+		fmt.Printf("No se pudo escribir en el archivo registro.txt debido a que hubo un error de consistencia detectado por Monotonic Writes \n")
 	}
-
 }
 
 func enviarComandoActualizarValor(client pb.MiServicioClient, nombreSector, nombreBase string, nuevoValor float32) {
@@ -161,22 +236,28 @@ func enviarComandoActualizarValor(client pb.MiServicioClient, nombreSector, nomb
 		}
 		PrimeraEscritura = false
 	}
-	//Escritura en el registro.txt
 	reloj := respFulcrum.Mensaje
-	logfile, err := os.OpenFile("registro.txt", os.O_APPEND|os.O_WRONLY, 0644)
-	if err != nil {
-		fmt.Printf("Log de registro no pudo abrirse exitosamente")
-	}
-	defer logfile.Close()
+	//Metodo Monotonic Writes
+	monotonic := MonotonicWrites(req.NombreSector, reloj, puerto)
+	if monotonic == "No hay problemas" {
+		//Escritura en el registro.txt
+		logfile, err := os.OpenFile("registro.txt", os.O_APPEND|os.O_WRONLY, 0644)
+		if err != nil {
+			fmt.Printf("Log de registro no pudo abrirse exitosamente")
+		}
+		defer logfile.Close()
 
-	_, err = fmt.Fprintf(logfile, "ActualizarValor %s %s %.0f %s %s\n", req.NombreSector, req.NombreBase, req.NuevoValor, reloj, puerto)
-	if err != nil {
-		fmt.Printf("No pudo escribirse correctamente en archivo log")
-	}
-	if resp.Exitoso {
-		fmt.Printf("Respuesta del %s: Comando ActualizarValor Ejecutado con éxito\n", fulcrum)
+		_, err = fmt.Fprintf(logfile, "ActualizarValor %s %s %.0f %s %s\n", req.NombreSector, req.NombreBase, req.NuevoValor, reloj, puerto)
+		if err != nil {
+			fmt.Printf("No pudo escribirse correctamente en archivo log")
+		}
+		if resp.Exitoso {
+			fmt.Printf("Respuesta del %s: Comando ActualizarValor Ejecutado con éxito\n", fulcrum)
+		} else {
+			fmt.Printf("Respuesta del %s: Comando ActualizarValor no pudo ser ejecutado\n", fulcrum)
+		}
 	} else {
-		fmt.Printf("Respuesta del %s: Comando ActualizarValor no pudo ser ejecutado\n", fulcrum)
+		fmt.Printf("No se pudo escribir en el archivo registro.txt debido a que hubo un error de consistencia detectado por Monotonic Writes \n")
 	}
 
 }
@@ -211,22 +292,28 @@ func enviarComandoBorrarBase(client pb.MiServicioClient, nombreSector, nombreBas
 		}
 		PrimeraEscritura = false
 	}
-	//Escritura en el registro.txt
 	reloj := respFulcrum.Mensaje
-	logfile, err := os.OpenFile("registro.txt", os.O_APPEND|os.O_WRONLY, 0644)
-	if err != nil {
-		fmt.Printf("Log de registro no pudo abrirse exitosamente")
-	}
-	defer logfile.Close()
+	//Metodo Monotonic Writes
+	monotonic := MonotonicWrites(req.NombreSector, reloj, puerto)
+	if monotonic == "No hay problemas" {
+		//Escritura en el registro.txt
+		logfile, err := os.OpenFile("registro.txt", os.O_APPEND|os.O_WRONLY, 0644)
+		if err != nil {
+			fmt.Printf("Log de registro no pudo abrirse exitosamente")
+		}
+		defer logfile.Close()
 
-	_, err = fmt.Fprintf(logfile, "BorrarBase %s %s %s %s\n", req.NombreSector, req.NombreBase, reloj, puerto)
-	if err != nil {
-		fmt.Printf("No pudo escribirse correctamente en archivo log")
-	}
-	if resp.Exitoso {
-		fmt.Printf("Respuesta del %s: Comando BorrarBase Ejecutado con éxito\n", fulcrum)
+		_, err = fmt.Fprintf(logfile, "BorrarBase %s %s %s %s\n", req.NombreSector, req.NombreBase, reloj, puerto)
+		if err != nil {
+			fmt.Printf("No pudo escribirse correctamente en archivo log")
+		}
+		if resp.Exitoso {
+			fmt.Printf("Respuesta del %s: Comando BorrarBase Ejecutado con éxito\n", fulcrum)
+		} else {
+			fmt.Printf("Respuesta del %s: Comando BorrarBase no pudo ser ejecutado\n", fulcrum)
+		}
 	} else {
-		fmt.Printf("Respuesta del %s: Comando BorrarBase no pudo ser ejecutado\n", fulcrum)
+		fmt.Printf("No se pudo escribir en el archivo registro.txt debido a que hubo un error de consistencia detectado por Monotonic Writes \n")
 	}
 }
 
